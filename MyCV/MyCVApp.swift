920db6f39dea92674ef84584982cfb313ccf5496
arlindaislami <arlindaaislamii@gmail.com>
2024-12-30 21:36:18 +0100

    Initial Commit

---
+++ MyCV/MyCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCVApp.swift
+//  MyCV
+//
+//  Created by Arlinda Islami on 2024-12-30.
+//
+
+import SwiftUI
+
+@main
+struct MyCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
